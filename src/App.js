9617a0fca682da669df886dd1783e13d11552470
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-30 17:20:01 -0300

    alteração no arquivo app.js correção nas rotas.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
 import ViewEmployeeComponent from './components/ViewEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <HeaderComponent/>
-        <div className="container">
-          <switch>
-            <Route path = "/" exact component = {ListEmployeeComponent}></Route>
-            <Route path = "/employees" component = {ListEmployeeComponent}></Route>
-            <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
-            <Route path = "/view-emplyee/:id" component = {ViewEmployeeComponent}></Route>
-          </switch>
-        </div>
-        <FooterComponent  />
-      </Router>
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Switch> 
+                        <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                        <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+                        <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
+                        <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
     </div>
-
+    
   );
 }
 
